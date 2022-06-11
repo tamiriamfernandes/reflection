31441b84784ca0b5ce9a9515e8770740f62c8c3a
Tamiriam Fernandes <tamiriam.fernandes@hotmail.com>
2022-06-11 00:22:16 -0300

    gitignore and initial

---
+++ ByteBank.Portal/Infraestrutura/WebApplication.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal.Infraestrutura
+{
+    public  class WebApplication
+    {
+        private readonly string[] _prefixos;
+        public WebApplication(string[] prefixos)
+        {
+            if(prefixos == null)
+                throw new ArgumentNullException(nameof(prefixos));
+
+            _prefixos = prefixos;
+        }
+        public void Iniciar()
+        {
+            var httpListener = new HttpListener();
+            httpListener.Start();
+
+
+            foreach (var prefixo in _prefixos)
+                httpListener.Prefixes.Add(prefixo);
+
+            var contexto = httpListener.GetContext();
+            var requisicao = contexto.Request;
+            var resposta = contexto.Response;
+
+            var respostaTexto = "Hello World";
+            var respostaConteudoBytes = Encoding.UTF8.GetBytes(respostaTexto);
+
+            resposta.ContentType = "text/html; charset=utf-8";
+            resposta.StatusCode = 200;
+            resposta.ContentLength64 = respostaConteudoBytes.Length;
+
+            resposta.OutputStream.Write(respostaConteudoBytes, 0, respostaConteudoBytes.Length);
+            resposta.OutputStream.Close();
+
+            httpListener.Stop();
+
+        }
+    }
+}
---
+++ ByteBank.Portal/Program.cs
@@ -0,0 +1,19 @@
+﻿using ByteBank.Portal.Infraestrutura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var prefixos = new string[] { "http://localhost:5341/" };
+            var webApllication = new WebApplication(prefixos);
+            webApllication.Iniciar();
+        }
+    }
+}
